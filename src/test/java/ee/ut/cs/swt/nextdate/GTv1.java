70ff95eee6ebe194d18e8f32dbd39eaa7502e566
frantiko <frantiko@ut.ee>
2025-03-26 09:58:30 +0200

    Task 3

--- src/test/java/ee/ut/cs/swt/nextdate/GTv1.java
+++ src/test/java/ee/ut/cs/swt/nextdate/GTv1.java
@@ -6,149 +6,149 @@ import static org.junit.Assert.*;
 public class GTv1 {
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate1() throws Throwable {
-	    NextDate nextDate0 = new NextDate(10, 10, 10);
-	    String string0 = nextDate0.run(10, 31, 1970);
+	public void testCreatesNDv21() throws Throwable {
+	    NDv2 NDv20 = new NDv2(10, 10, 10);
+	    String string0 = NDv20.run(10, 31, 1970);
 	    assertEquals("11/1/1970", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate0() throws Throwable {
-	    NextDate nextDate0 = new NextDate(12, 12, 12);
-	    String string0 = nextDate0.run(12, 1839, 1839);
+	public void testCreatesNDv20() throws Throwable {
+	    NDv2 NDv20 = new NDv2(12, 12, 12);
+	    String string0 = NDv20.run(12, 1839, 1839);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate3() throws Throwable {
-	    NextDate nextDate0 = new NextDate(4, 4, 4);
-	    String string0 = nextDate0.run(4, 31, 1970);
+	public void testCreatesNDv23() throws Throwable {
+	    NDv2 NDv20 = new NDv2(4, 4, 4);
+	    String string0 = NDv20.run(4, 31, 1970);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate2() throws Throwable {
-	    NextDate nextDate0 = new NextDate(12, 12, 12);
-	    String string0 = nextDate0.run(5, 12, 1906);
+	public void testCreatesNDv22() throws Throwable {
+	    NDv2 NDv20 = new NDv2(12, 12, 12);
+	    String string0 = NDv20.run(5, 12, 1906);
 	    assertEquals("5/13/1906", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate5() throws Throwable {
-	    NextDate nextDate0 = new NextDate(6, 6, 6);
-	    String string0 = nextDate0.run(6, 30, 1899);
+	public void testCreatesNDv25() throws Throwable {
+	    NDv2 NDv20 = new NDv2(6, 6, 6);
+	    String string0 = NDv20.run(6, 30, 1899);
 	    assertEquals("7/1/1899", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate4() throws Throwable {
-	    NextDate nextDate0 = new NextDate(11, 11, 11);
-	    String string0 = nextDate0.run(11, 11, 1880);
+	public void testCreatesNDv24() throws Throwable {
+	    NDv2 NDv20 = new NDv2(11, 11, 11);
+	    String string0 = NDv20.run(11, 11, 1880);
 	    assertEquals("11/12/1880", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunReturningNonEmptyString() throws Throwable {
-	    NextDate nextDate0 = new NextDate(2, 2, 2);
-	    String string0 = nextDate0.run(2, 31, 1901);
+	    NDv2 NDv20 = new NDv2(2, 2, 2);
+	    String string0 = NDv20.run(2, 31, 1901);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithNegative() throws Throwable {
-	    NextDate nextDate0 = new NextDate((-112), (-112), (-112));
-	    String string0 = nextDate0.run((-112), (-112), (-112));
+	    NDv2 NDv20 = new NDv2((-112), (-112), (-112));
+	    String string0 = NDv20.run((-112), (-112), (-112));
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithNegativeAndNegative() throws Throwable {
-	    NextDate nextDate0 = new NextDate(12, 12, 12);
-	    String string0 = nextDate0.run((-441), 12, (-441));
+	    NDv2 NDv20 = new NDv2(12, 12, 12);
+	    String string0 = NDv20.run((-441), 12, (-441));
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithPositive1() throws Throwable {
-	    NextDate nextDate0 = new NextDate(12, 12, 12);
-	    String string0 = nextDate0.run(12, 12, 12);
+	    NDv2 NDv20 = new NDv2(12, 12, 12);
+	    String string0 = NDv20.run(12, 12, 12);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate14() throws Throwable {
-	    NextDate nextDate0 = new NextDate(9, 9, 9);
-	    String string0 = nextDate0.run(9, 31, 1915);
+	public void testCreatesNDv214() throws Throwable {
+	    NDv2 NDv20 = new NDv2(9, 9, 9);
+	    String string0 = NDv20.run(9, 31, 1915);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithPositive0() throws Throwable {
-	    NextDate nextDate0 = new NextDate(290, 290, 290);
-	    String string0 = nextDate0.run(290, 290, 290);
+	    NDv2 NDv20 = new NDv2(290, 290, 290);
+	    String string0 = NDv20.run(290, 290, 290);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate13() throws Throwable {
-	    NextDate nextDate0 = new NextDate(8, 8, 8);
-	    String string0 = nextDate0.run(8, 8, 1826);
+	public void testCreatesNDv213() throws Throwable {
+	    NDv2 NDv20 = new NDv2(8, 8, 8);
+	    String string0 = NDv20.run(8, 8, 1826);
 	    assertEquals("8/9/1826", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithPositive2() throws Throwable {
-	    NextDate nextDate0 = new NextDate(1, 1, 1);
-	    String string0 = nextDate0.run(1, 1, 2022);
+	    NDv2 NDv20 = new NDv2(1, 1, 1);
+	    String string0 = NDv20.run(1, 1, 2022);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate7() throws Throwable {
-	    NextDate nextDate0 = new NextDate(7, 7, 7);
-	    String string0 = nextDate0.run(7, 31, 1930);
+	public void testCreatesNDv27() throws Throwable {
+	    NDv2 NDv20 = new NDv2(7, 7, 7);
+	    String string0 = NDv20.run(7, 31, 1930);
 	    assertEquals("7/31/1930", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate6() throws Throwable {
-	    NextDate nextDate0 = new NextDate(12, 12, 12);
-	    String string0 = nextDate0.run(12, 12, 1839);
+	public void testCreatesNDv26() throws Throwable {
+	    NDv2 NDv20 = new NDv2(12, 12, 12);
+	    String string0 = NDv20.run(12, 12, 1839);
 	    assertEquals("12/13/1839", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate9() throws Throwable {
-	    NextDate nextDate0 = new NextDate(2, 2, 2);
-	    String string0 = nextDate0.run(2, 28, 1879);
+	public void testCreatesNDv29() throws Throwable {
+	    NDv2 NDv20 = new NDv2(2, 2, 2);
+	    String string0 = NDv20.run(2, 28, 1879);
 	    assertEquals("3/1/1879", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate8() throws Throwable {
-	    NextDate nextDate0 = new NextDate(2, 2, 2);
-	    String string0 = nextDate0.run(2, 2, 1819);
+	public void testCreatesNDv28() throws Throwable {
+	    NDv2 NDv20 = new NDv2(2, 2, 2);
+	    String string0 = NDv20.run(2, 2, 1819);
 	    assertEquals("2/3/1819", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate10() throws Throwable {
-	    NextDate nextDate0 = new NextDate(2, 2, 2);
-	    String string0 = nextDate0.run(2, 29, 1899);
+	public void testCreatesNDv210() throws Throwable {
+	    NDv2 NDv20 = new NDv2(2, 2, 2);
+	    String string0 = NDv20.run(2, 29, 1899);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate12() throws Throwable {
-	    NextDate nextDate0 = new NextDate(3, 3, 3);
-	    String string0 = nextDate0.run(3, 3, 1826);
+	public void testCreatesNDv212() throws Throwable {
+	    NDv2 NDv20 = new NDv2(3, 3, 3);
+	    String string0 = NDv20.run(3, 3, 1826);
 	    assertEquals("3/4/1826", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNextDate11() throws Throwable {
-	    NextDate nextDate0 = new NextDate(1, 1, 1);
-	    String string0 = nextDate0.run(1, 1, 2020);
+	public void testCreatesNDv211() throws Throwable {
+	    NDv2 NDv20 = new NDv2(1, 1, 1);
+	    String string0 = NDv20.run(1, 1, 2020);
 	    assertEquals("1/2/2020", string0);
 	}
 
--- src/test/java/ee/ut/cs/swt/nextdate/GTv12.java
+++ src/test/java/ee/ut/cs/swt/nextdate/GTv12.java
@@ -6,163 +6,163 @@ import static org.junit.Assert.*;
 public class GTv12 {
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv128() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(7, 7, 7);
-	    String string0 = nDv12_0.run(7, 7, 1805);
+	public void testCreatesNDv28() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(7, 7, 7);
+	    String string0 = NDv2_0.run(7, 7, 1805);
 	    assertEquals("7/7/1805", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv129() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 2, 2);
-	    String string0 = nDv12_0.run(2, 2, 1826);
+	public void testCreatesNDv29() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(2, 2, 2);
+	    String string0 = NDv2_0.run(2, 2, 1826);
 	    assertEquals("2/3/1826", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv126() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(12, 12, 12);
-	    String string0 = nDv12_0.run(12, 12, 1812);
+	public void testCreatesNDv26() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(12, 12, 12);
+	    String string0 = NDv2_0.run(12, 12, 1812);
 	    assertEquals("12/13/1812", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv127() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 31, 31);
-	    String string0 = nDv12_0.run(12, 31, 2021);
+	public void testCreatesNDv27() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(2, 31, 31);
+	    String string0 = NDv2_0.run(12, 31, 2021);
 	    assertEquals("Invalid Next Year", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv124() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(11, 30, 30);
-	    String string0 = nDv12_0.run(11, 30, 1840);
+	public void testCreatesNDv24() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(11, 30, 30);
+	    String string0 = NDv2_0.run(11, 30, 1840);
 	    assertEquals("12/1/1840", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv125() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 29, 29);
-	    String string0 = nDv12_0.run(2, 29, 1826);
+	public void testCreatesNDv25() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(2, 29, 29);
+	    String string0 = NDv2_0.run(2, 29, 1826);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunReturningNonEmptyString() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 2, 2);
-	    String string0 = nDv12_0.run(12, 31, 1991);
+	    NDv2 NDv2_0 = new NDv2(2, 2, 2);
+	    String string0 = NDv2_0.run(12, 31, 1991);
 	    assertEquals("1/1/1992", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv122() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(5, 5, 5);
-	    String string0 = nDv12_0.run(5, 5, 1826);
+	public void testCreatesNDv22() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(5, 5, 5);
+	    String string0 = NDv2_0.run(5, 5, 1826);
 	    assertEquals("5/6/1826", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1215() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(4, 4, 4);
-	    String string0 = nDv12_0.run(4, 4, 1805);
+	public void testCreatesNDv215() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(4, 4, 4);
+	    String string0 = NDv2_0.run(4, 4, 1805);
 	    assertEquals("4/5/1805", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv123() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(6, 31, 6);
-	    String string0 = nDv12_0.run(6, 31, 1844);
+	public void testCreatesNDv23() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(6, 31, 6);
+	    String string0 = NDv2_0.run(6, 31, 1844);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1216() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(9, 9, 9);
-	    String string0 = nDv12_0.run(9, 9, 1838);
+	public void testCreatesNDv216() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(9, 9, 9);
+	    String string0 = NDv2_0.run(9, 9, 1838);
 	    assertEquals("9/10/1838", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithNegative() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12((-2867), (-2867), (-2867));
-	    String string0 = nDv12_0.run((-2867), (-2867), (-2867));
+	    NDv2 NDv2_0 = new NDv2((-2867), (-2867), (-2867));
+	    String string0 = NDv2_0.run((-2867), (-2867), (-2867));
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv120() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(8, 8, 8);
-	    String string0 = nDv12_0.run(8, 1876, 1876);
+	public void testCreatesNDv20() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(8, 8, 8);
+	    String string0 = NDv2_0.run(8, 1876, 1876);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv121() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 2, 2);
-	    String string0 = nDv12_0.run(1, 31, 1991);
+	public void testCreatesNDv21() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(2, 2, 2);
+	    String string0 = NDv2_0.run(1, 31, 1991);
 	    assertEquals("2/1/1991", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithZero() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(0, 0, 0);
-	    String string0 = nDv12_0.run(0, 6, 6);
+	    NDv2 NDv2_0 = new NDv2(0, 0, 0);
+	    String string0 = NDv2_0.run(0, 6, 6);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithPositive1() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(12, 12, 12);
-	    String string0 = nDv12_0.run(12, 12, 12);
+	    NDv2 NDv2_0 = new NDv2(12, 12, 12);
+	    String string0 = NDv2_0.run(12, 12, 12);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithPositive0() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2021, 2021, 2021);
-	    String string0 = nDv12_0.run(2021, 2021, 2021);
+	    NDv2 NDv2_0 = new NDv2(2021, 2021, 2021);
+	    String string0 = NDv2_0.run(2021, 2021, 2021);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
 	public void testRunWithPositive2() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(12, 12, 12);
-	    String string0 = nDv12_0.run(12, 2193, 2193);
+	    NDv2 NDv2_0 = new NDv2(12, 12, 12);
+	    String string0 = NDv2_0.run(12, 2193, 2193);
 	    assertEquals("invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1210() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 28, 28);
-	    String string0 = nDv12_0.run(2, 28, 1826);
+	public void testCreatesNDv210() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(2, 28, 28);
+	    String string0 = NDv2_0.run(2, 28, 1826);
 	    assertEquals("3/1/1826", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1211() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(2, 30, 30);
-	    String string0 = nDv12_0.run(2, 30, 1826);
+	public void testCreatesNDv211() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(2, 30, 30);
+	    String string0 = NDv2_0.run(2, 30, 1826);
 	    assertEquals("Invalid Input Date", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1212() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(3, 3, 3);
-	    String string0 = nDv12_0.run(3, 3, 1840);
+	public void testCreatesNDv212() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(3, 3, 3);
+	    String string0 = NDv2_0.run(3, 3, 1840);
 	    assertEquals("3/4/1840", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1213() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(8, 8, 8);
-	    String string0 = nDv12_0.run(8, 8, 1845);
+	public void testCreatesNDv213() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(8, 8, 8);
+	    String string0 = NDv2_0.run(8, 8, 1845);
 	    assertEquals("8/9/1845", string0);
 	}
 
 	@Test(timeout = 4000)
-	public void testCreatesNDv1214() throws Throwable {
-	    NDv12 nDv12_0 = new NDv12(10, 10, 10);
-	    String string0 = nDv12_0.run(10, 10, 1826);
+	public void testCreatesNDv214() throws Throwable {
+	    NDv2 NDv2_0 = new NDv2(10, 10, 10);
+	    String string0 = NDv2_0.run(10, 10, 1826);
 	    assertEquals("10/11/1826", string0);
 	}
 
